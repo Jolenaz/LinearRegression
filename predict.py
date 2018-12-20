e449c3119ba556797587cbe376be7be7054aebb3
jonas <jonasbellessa@gmail.com>
2018-12-20 13:29:52 +0100

    linearReg

---
+++ predict.py
@@ -0,0 +1,17 @@
+import sys
+import os.path
+
+if os.path.isfile("coef.txt"):
+    fd = open("coef.txt", "r+")
+    A = fd.readline().split(" ")
+else:
+    A = [0,0]
+
+print "Enter a mileage: "
+
+data = sys.stdin.readline()
+
+print "The predicted value of your car is:"
+print (int(float(data) * float(A[0]) + float(A[1])))
+
+
