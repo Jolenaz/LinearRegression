429efc828806818366b5d3ea830d62ef8490bc4b
jonas <jonasbellessa@gmail.com>
2019-01-11 10:33:09 +0100

    finished

--- learn.py
+++ learn.py
@@ -43,6 +43,7 @@ for line in data:
 
 m = len(parsedData)
 
+print ("method des moindres carres")
 x = [ it[0] for it in parsedData ]
 y = [ it[1] for it in parsedData ]
 xy = [ (it[1] * it[0]) for it in parsedData ]
@@ -50,23 +51,46 @@ x2 = [ (it[0] * it[0]) for it in parsedData ]
 xm = sum(x) / m
 ym = sum(y) / m
 
-Sigxy = [ ((it[1] - ym) * (it[0] - xm))  for it in parsedData ]
-Sigx  = [ ((it[0] - xm) * (it[0] - xm))  for it in parsedData ]
-Sigy  = [ ((it[1] - ym) * (it[1] - ym))  for it in parsedData ]
 
 a = float(m * sum(xy) - sum(x) * sum(y) ) / float(m * sum(x2) - sum(x) * sum(x))
 b = ym - a * xm 
+
+print ("a " + str(a))
+print ("b " + str(b))
+
+t1 = 0
+t0 = float(sum(y)) / float(m)
+
+print ("method iterative")
+
+i = 0 
+while (i < 100000): 
+    tmp0 = 1 / float(m) * sum([ (t0 + t1 * it[0] - it[1]) for it in parsedData ]) 
+    tmp1 = 1 / float(m) * sum([ ((t0 + t1 * it[0] - it[1]) * it[0]) for it in parsedData ])
+    t0 -= 0.001 * tmp0    
+    t1 -= 0.0000000001 * tmp1    
+    i += 1
+
+print ("t1  " + str(t1))
+print ("t0  " + str(t0))
+
+Sigxy = [ ((it[1] - ym) * (it[0] - xm))  for it in parsedData ]
+Sigx  = [ ((it[0] - xm) * (it[0] - xm))  for it in parsedData ]
+Sigy  = [ ((it[1] - ym) * (it[1] - ym))  for it in parsedData ]
 
 r2 = sum(Sigxy) / math.sqrt(sum(Sigx) * sum(Sigy))
 
 X = np.linspace(min(x), max(x), 1000)
 
-plt.scatter(x, y)
-plt.plot(X, a*X+b)
+plt.scatter(x, y, label='Data')
+plt.plot(X, a*X+b, 'b-', label='m2')
+plt.plot(X, t1*X+t0, 'g-', label='iter')
 
 plt.xlabel(xLabel)
 plt.ylabel(yLabel)
 plt.title("Linear Reg")
+
+plt.legend(loc='best')
 
 plt.show()
 
